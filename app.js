a1a357aabbabf336de46a3ae737a5bad98d011a7
Anar Seyf <anar.seyf@gmail.com>
2020-05-02 14:59:17 -0700

    Static

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('static'))
+
+app.get('/', (req, res) => res.send('Hello World?'))
+app.get('/test', (req, res) => res.send('TEST'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
