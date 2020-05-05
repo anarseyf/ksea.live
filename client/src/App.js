d17279b85f422947085d92b08ced2e177b1922e4
Anar Seyf <anar.seyf@gmail.com>
2020-05-05 16:42:34 -0700

    CSS modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import styles from "./app.module.css";
 import { getData } from "./logic";
 
-function updateData() {
-  getData().then((data) => console.log("client: ", data));
-}
-
 function App() {
+  const [data, setData] = useState({});
+
+  function updateData() {
+    getData()
+      .then(setData)
+      .catch((error) => setData({ error }));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>OK then</p>
-        <button onClick={() => updateData()}>get data</button>
+        <button className={styles.button} onClick={updateData}>
+          Fetch data
+        </button>
+        <table>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+          </tr>
+          {Object.keys(data).map((key) => (
+            <tr>
+              <td>{key}</td>
+              <td>{data[key]}</td>
+            </tr>
+          ))}
+        </table>
       </header>
     </div>
   );
