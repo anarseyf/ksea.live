8607101bedf66d85b8bfb9daef7562d6dd71adf3
Anar Seyf <anar.seyf@gmail.com>
2020-05-06 20:53:47 -0700

    Dev vs Prod

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3001;
 
 app.use(express.static("static"));
 
+app.set("port", process.env.PORT || port);
+
+const staticPath =
+  process.env.NODE_ENV === "production" ? "client/build" : "client/src";
+app.use(express.static(path.join(__dirname, staticPath)));
+
 app.get("/", (req, res) => res.send("Hello World?"));
 
+app.get("/api/env", (req, res) => {
+  res.json({ nodeEnv: process.env.NODE_ENV || "" });
+});
+
 app.get("/api/data", (req, res) => {
-  console.log("API/DATA call");
   const data = { payload: 42 };
   res.json(data);
 });
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`${path.basename(__filename)} listening at :${port}`)
 );
