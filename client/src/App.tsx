28ddfb088e1bd4a8be841c015aefa293cfa86d75
Anar Seyf <anar.seyf@gmail.com>
2020-05-19 17:12:56 -0700

    Semiotic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,66 +6,69 @@ import styles from "./app.module.css";
 import { getEnv, getSeattle911 } from "./logic";
 import { Chart } from "./Chart";
 import { DataTable } from "./DataTable";
+import Scatterplot from "./components/Scatterplot";
 
 function App() {
-  const initialData: object[] = [
-    {
-      address: "15TH AV E / E REPUBLICAN ST",
-      type: "Aid Response",
-      datetime: "2019-11-23T15:41:00.000",
-      latitude: "47.623102",
-      longitude: "-122.312631",
-      incident_number: "F0001",
-    },
-  ];
-  const [data, setData] = useState(initialData);
+    const initialData: object[] = [
+        {
+            address: "15TH AV E / E REPUBLICAN ST",
+            type: "Aid Response",
+            datetime: "2019-11-23T15:41:00.000",
+            latitude: "47.623102",
+            longitude: "-122.312631",
+            incident_number: "F0001",
+        },
+    ];
+    const [data, setData] = useState(initialData);
 
-  function updateEnv() {
-    getEnv()
-      .then((data) =>
-        setData([
-          {
-            NODE_ENV: data.NODE_ENV,
-          },
-        ])
-      )
-      .catch((error) => setData([{ error }]));
-  }
+    function updateEnv() {
+        getEnv()
+            .then((data) =>
+                setData([
+                    {
+                        NODE_ENV: data.NODE_ENV,
+                    },
+                ])
+            )
+            .catch((error) => setData([{ error }]));
+    }
 
-  function updateData() {
-    getSeattle911()
-      .then((data) => {
-        console.log("Data from API: ", data);
-        setData(data);
-      })
-      .catch((error) => setData([{ error }]));
-  }
+    function updateData() {
+        getSeattle911()
+            .then((data) => {
+                console.log("Data from API: ", data);
+                setData(data);
+            })
+            .catch((error) => setData([{ error }]));
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
 
-        <Chart></Chart>
+                <Chart></Chart>
 
-        <Button
-          variant="danger"
-          className={`${styles.button} m-2`}
-          onClick={updateEnv}
-        >
-          Get env
-        </Button>
-        <Button
-          className={`${styles.button} m-2`}
-          variant="danger"
-          onClick={updateData}
-        >
-          Get data
-        </Button>
-        <DataTable data={data}></DataTable>
-      </header>
-    </div>
-  );
+                <Scatterplot></Scatterplot>
+
+                <Button
+                    variant="danger"
+                    className={`${styles.button} m-2`}
+                    onClick={updateEnv}
+                >
+                    Get env
+                </Button>
+                <Button
+                    className={`${styles.button} m-2`}
+                    variant="danger"
+                    onClick={updateData}
+                >
+                    Get data
+                </Button>
+                <DataTable data={data}></DataTable>
+            </header>
+        </div>
+    );
 }
 
 export default App;
