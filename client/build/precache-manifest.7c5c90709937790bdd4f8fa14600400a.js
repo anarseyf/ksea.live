827e628e4dfbd023fd3709b6169d28eeb1d6808e
Anar Seyf <anar.seyf@gmail.com>
2020-05-20 17:29:58 -0700

    leaflet build

---
+++ client/build/precache-manifest.7c5c90709937790bdd4f8fa14600400a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41593ef8ebd211bf5416a94f95f72bab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fdc7f067bc9e4fdbfcb",
+    "url": "/static/css/2.b756e824.chunk.css"
+  },
+  {
+    "revision": "14a50ce857c52adf0e11",
+    "url": "/static/css/main.cc1fafe0.chunk.css"
+  },
+  {
+    "revision": "6fdc7f067bc9e4fdbfcb",
+    "url": "/static/js/2.10740a9f.chunk.js"
+  },
+  {
+    "revision": "421a6b0bcd5694a58bfc2217b9ad4439",
+    "url": "/static/js/2.10740a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a50ce857c52adf0e11",
+    "url": "/static/js/main.71dbb5f6.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3758b08dd006ce85dfd00f7defa202e8.js"
+  "/precache-manifest.7c5c90709937790bdd4f8fa14600400a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
