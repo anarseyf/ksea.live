f1442a63a7f2d80558c842bb1766a32d587e8bd4
Anar Seyf <anar.seyf@gmail.com>
2020-05-20 20:26:41 -0700

    Reach router

---
+++ client/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Router, Link } from "@reach/router";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import { CoffeeMap } from "./maps/CoffeeMap";
+import { KitchenSink } from "./components/KitchenSink";
+
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar bg="dark">
+                <Nav>
+                    <Nav.Link>
+                        <Link to="/">Home</Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link to="/map">Map</Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link to="/kitchensink">Charts</Link>
+                    </Nav.Link>
+                </Nav>
+            </Navbar>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Router>
+                <CoffeeMap path="/map"></CoffeeMap>
+                <KitchenSink path="/kitchensink"></KitchenSink>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
