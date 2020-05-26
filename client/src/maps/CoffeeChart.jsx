b8e1daaef4e8452bac8994972f32265daba45f1d
Anar Seyf <anar.seyf@gmail.com>
2020-05-26 16:11:59 -0700

    Histogram with axes

--- client/src/maps/CoffeeChart.jsx
+++ client/src/maps/CoffeeChart.jsx
@@ -8,23 +8,34 @@ export function CoffeeChart() {
 
     const [svgData, setSvgData] = useState([]);
 
-    const svgWidth = 500,
+    const svgWidth = 200,
         svgHeight = 80,
-        margin = { bottom: 20, left: 30 },
-        width = svgWidth - margin.left,
-        height = svgHeight - margin.bottom;
+        margin = { top: 10, right: 10, bottom: 20, left: 30 },
+        width = svgWidth - margin.left - margin.right,
+        height = svgHeight - margin.bottom - margin.top;
 
     const xAxisRef = useRef(null);
+    const yAxisRef = useRef(null);
 
     useEffect(() => {
+        if (!tweets.length) {
+            return;
+        }
+
         const data = tweets.map(({ created_at, derived: { lat } }) => ({
             timestamp: +new Date(created_at),
             value: lat,
         }));
 
+        const actualTimeExtent = d3.extent(data, (d) => d.timestamp);
+        const expandedTimeExtent = [
+            d3.timeHour.offset(actualTimeExtent[0], -1),
+            d3.timeHour.offset(actualTimeExtent[1], 1),
+        ];
+
         const xScale = d3
             .scaleTime()
-            .domain(d3.extent(data, (d) => d.timestamp))
+            .domain(expandedTimeExtent)
             .range([0, width]);
 
         const histogram = d3
@@ -34,51 +45,67 @@ export function CoffeeChart() {
             .thresholds(d3.timeHours(...xScale.domain(), 1));
 
         const bins = histogram(data);
-        console.log("BINS", bins);
 
         const yScale = d3
             .scaleLinear()
             .domain([0, d3.max(bins, (b) => b.length)])
             .range([height, 0]);
 
+        const dateFormatter = d3.timeFormat("%H:%M"); // "(%b %d) %H:%M"
+
+        console.log(
+            "BINS",
+            bins.map((b) => b.length)
+        );
+
         const xAxis = d3
             .axisBottom()
-            .tickFormat(d3.timeFormat("%H:%M"))
-            .scale(xScale);
+            .tickFormat(dateFormatter)
+            .scale(xScale)
+            .ticks(d3.timeHour.every(12));
         d3.select(xAxisRef.current).call(xAxis);
 
-        const binWidth = Math.floor(width / bins.length) - 2;
+        const yAxis = d3.axisLeft().scale(yScale).ticks(2);
+        d3.select(yAxisRef.current).call(yAxis);
+
+        const firstRealBin = bins[1];
+        const binWidth = Math.floor(
+            xScale(firstRealBin.x1) - xScale(firstRealBin.x0)
+        );
 
         const newSvgData = bins.map(({ x0, x1, length }) => ({
-            x: xScale(x0),
+            x: xScale(x0) - binWidth / 2,
             width: binWidth,
             y: yScale(length),
             height: yScale(0) - yScale(length),
             rx: binWidth / 4,
         }));
 
-        console.log(newSvgData[0]);
-
         setSvgData(newSvgData);
     }, [tweets]);
 
     return (
         <svg className={styles.smallchart} width={svgWidth} height={svgHeight}>
-            {svgData.map((d) => (
-                <rect
-                    x={d.x}
-                    y={d.y}
-                    width={d.width}
-                    height={d.height}
-                    rx={d.rx}
-                    fill="green"
-                ></rect>
-            ))}
+            <g transform={`translate(${margin.left},${margin.top})`}>
+                {svgData.map((d) => (
+                    <rect
+                        x={d.x}
+                        y={d.y}
+                        width={d.width}
+                        height={d.height}
+                        rx={d.rx}
+                        fill="green"
+                    ></rect>
+                ))}
+            </g>
             <g
                 ref={xAxisRef}
-                transform={`translate(${margin.left}, ${height})`}
+                transform={`translate(${margin.left},${margin.top + height})`}
             />
-            {/* <g ref="yAxis" transform={`translate(${margin.left}, 0)`} /> */}
+            <g
+                ref={yAxisRef}
+                transform={`translate(${margin.left},${margin.top})`}
+            />
         </svg>
     );
 }
