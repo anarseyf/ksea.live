2c8cb03db46faa4626be2f18ae92d3a92771d005
Anar Seyf <anar.seyf@gmail.com>
2020-06-01 11:05:27 -0700

    Map + Dot tweaks

--- client/src/dispatch/Dot.jsx
+++ client/src/dispatch/Dot.jsx
@@ -1,35 +1,55 @@
 import React from "react";
 import { Circle } from "react-leaflet";
+import App from "../App";
 
-const opacity = 0.7;
+export const Appearance = {
+  Normal: 0,
+  Dimmed: -1,
+  Highlighted: 1,
+};
 
-export function Dot({ coordinates = [51.477, 0], color = "dodgerblue" }) {
-    return (
-        <>
-            <Circle
-                center={coordinates}
-                radius={200}
-                color={color}
-                fill={true}
-                fillOpacity={opacity}
-                stroke={false}
-            ></Circle>
-            <Circle
-                center={coordinates}
-                radius={300}
-                weight={1}
-                color={color}
-                opacity={opacity}
-                fill={false}
-            ></Circle>
-            {/* <Circle
-                center={coordinates}
-                radius={600}
-                weight={1}
-                color={color}
-                opacity={opacity}
-                fill={false}
-            ></Circle> */}
-        </>
-    );
+export function Dot({
+  coordinates = [51.477, 0], // Greenwich, but why?
+  color = "dodgerblue",
+  appearance = Appearance.Normal,
+}) {
+  let opacity = 0.7;
+  if (appearance === Appearance.Highlighted) {
+    opacity = 0.9;
+  } else if (appearance === Appearance.Dimmed) {
+    opacity = 0.4;
+  }
+
+  return (
+    <>
+      <Circle
+        center={coordinates}
+        radius={200}
+        color={color}
+        fill={true}
+        fillOpacity={opacity}
+        stroke={false}
+      ></Circle>
+      {appearance >= Appearance.Normal && (
+        <Circle
+          center={coordinates}
+          radius={300}
+          weight={3}
+          color={color}
+          opacity={opacity}
+          fill={false}
+        ></Circle>
+      )}
+      {appearance >= Appearance.Highlighted && (
+        <Circle
+          center={coordinates}
+          radius={400}
+          weight={2}
+          color={color}
+          opacity={opacity}
+          fill={false}
+        ></Circle>
+      )}
+    </>
+  );
 }
--- client/src/dispatch/Map.jsx
+++ client/src/dispatch/Map.jsx
@@ -3,9 +3,10 @@ import { Map as LeafletMap, TileLayer, GeoJSON } from "react-leaflet";
 import zipcodes from "./zip-codes.json";
 import { centroid } from "./geojson";
 import "./map.css";
-import { Dot } from "./Dot";
+import { Dot, Appearance } from "./Dot";
 import { TweetsContext } from "./TweetsProvider";
 import { groupBy, GroupByOptions } from "../groupby";
+import { UserContext, UserContextKeys } from "./UserProvider";
 
 const minZoom = 10,
   maxZoom = 14;
@@ -20,7 +21,19 @@ const geojsonStyle = {
   weight: 2,
 };
 
+const tileOptions = {
+  url: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}",
+  ext: "png",
+  // accessToken:
+  //   "pk.eyJ1IjoiYW5hcnNleWYiLCJhIjoiY2thZXlra3llMGF4MDJ4cXYzY2ZkamVkdyJ9.K8CENC0jz2D0O6ziL_jnNg", // Mapbox: 'coffee' token
+  attribution:
+    'Tiles by <a href="http://stamen.com">Stamen</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Data &copy; <a href="http://www.openstreetmap.org/copyright">OSM</a>',
+};
+
 export function Map({ area }) {
+  const [user] = useContext(UserContext);
+  const selectedTweet = user[UserContextKeys.SelectedTweet];
+
   const [_, tweets] = useContext(TweetsContext);
 
   const areaFilter = ({ properties: { GEOID10 } }) => GEOID10 === area;
@@ -31,27 +44,51 @@ export function Map({ area }) {
       features: features.filter(areaFilter),
       ...rest,
     };
-
-    zoom = 12;
   }
 
-  let center = centroid(geojson.features);
+  if (area) {
+    zoom = 12;
+  }
+  if (selectedTweet) {
+    zoom = 13;
+  }
+
+  let center = selectedTweet
+    ? [selectedTweet.derived.lat, selectedTweet.derived.long]
+    : centroid(geojson.features);
   console.log("MAP/center", center);
 
   const tweetsByType = groupBy(GroupByOptions.IncidentType, tweets);
   const mapper = ({ color, values }) =>
-    values.map(({ derived: { lat, long } }) => ({ lat, long, color }));
+    values.map(({ id_str, derived: { lat, long } }) => ({
+      id_str,
+      lat,
+      long,
+      color,
+    }));
 
-  const data = tweetsByType.map(mapper).flat();
+  let data = tweetsByType.map(mapper).flat();
 
-  const options = {
-    url: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}",
-    ext: "png",
-    // accessToken:
-    //   "pk.eyJ1IjoiYW5hcnNleWYiLCJhIjoiY2thZXlra3llMGF4MDJ4cXYzY2ZkamVkdyJ9.K8CENC0jz2D0O6ziL_jnNg", // Mapbox: 'coffee' token
-    attribution:
-      'Tiles by <a href="http://stamen.com">Stamen</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Data &copy; <a href="http://www.openstreetmap.org/copyright">OSM</a>',
+  const isSelectedDot = ({ id_str }) => selectedTweet.id_str === id_str;
+  if (selectedTweet) {
+    // Render selected dot last, so it appears on top
+    const selectedIndex = data.findIndex(isSelectedDot);
+    data = [
+      ...data.slice(0, selectedIndex),
+      ...data.slice(selectedIndex + 1),
+      data[selectedIndex],
+    ];
+  }
+
+  const appearanceFn = (d) => {
+    return selectedTweet
+      ? isSelectedDot(d)
+        ? Appearance.Highlighted
+        : Appearance.Dimmed
+      : Appearance.Normal;
   };
+
+  console.log("MAP/rendering", data);
 
   return (
     <LeafletMap
@@ -61,12 +98,13 @@ export function Map({ area }) {
       maxZoom={maxZoom}
       zoomControl={false}
     >
-      <TileLayer {...options} />
+      <TileLayer {...tileOptions} />
       <GeoJSON data={geojson} style={geojsonStyle}></GeoJSON>
       {data.map((d) => (
         <Dot
           coordinates={[d.lat, d.long]}
           color={d.color || defaultColor}
+          appearance={appearanceFn(d)}
         ></Dot>
       ))}
     </LeafletMap>
