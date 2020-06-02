d6f68680ce503f2ec87ec77e2e342c6a54c7de44
Anar Seyf <anar.seyf@gmail.com>
2020-06-02 14:12:20 -0700

    Move logic to server WIP

--- client/src/dispatch/Tweet.jsx
+++ client/src/dispatch/Tweet.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
 import styles from "./tweet.module.scss";
-import { Mappers, GroupByOptions } from "../groupby";
 import { useLegend } from "./useLegend";
 import { UserContext, UserContextKeys } from "./UserProvider";
 
@@ -10,20 +9,13 @@ export function Tweet({ tweet }) {
   const [user, setSelection] = useContext(UserContext);
   const selectedTweet = user[UserContextKeys.SelectedTweet];
   const [isSelected, setIsSelected] = useState(false);
-  const legend = useLegend();
+  const [legend] = useLegend();
 
   useEffect(() => {
     setIsSelected(selectedTweet && selectedTweet.id_str === tweet.id_str);
   }, [selectedTweet]);
 
-  const groupedby = GroupByOptions.IncidentType;
-  const key = Mappers[groupedby](tweet);
   let color = "purple";
-
-  if (legend[groupedby]) {
-    const item = legend[groupedby].find((d) => d.key === key);
-    item && (color = item.color);
-  }
 
   const handleClick = () => {
     const newSelectedTweet =
@@ -44,7 +36,7 @@ export function Tweet({ tweet }) {
       </div>
       <div className={styles.location}>{tweet.derived.zip}</div>
       <div className={styles.type} style={{ color }}>
-        {key}
+        {"???"}
       </div>
     </div>
   );
--- client/src/groupby.js
+++ client/src/groupby.js
@@ -10,33 +10,7 @@ export const GroupByOptions = {
 
 export const DefaultInterval = 12 * 3600 * 1000;
 
-const IncidentTypes = {
-  Known: { Fire: "fire", Medic: "medic", Aid: "aid" },
-  Default: "other",
-};
-
-const RequiredKeys = {
-  [GroupByOptions.IncidentType]: [
-    IncidentTypes.Known.Fire,
-    IncidentTypes.Known.Medic,
-    IncidentTypes.Known.Aid,
-    IncidentTypes.Default,
-  ],
-};
-
 export const Mappers = {
-  [GroupByOptions.IncidentType]: ({ derived: { description } }) => {
-    const options = Object.values(IncidentTypes.Known);
-    const desc = description.toLowerCase();
-    const match = options.reduce((matchedOption, option) => {
-      if (matchedOption) {
-        return matchedOption;
-      }
-      return desc.includes(option) ? option : undefined;
-    }, undefined);
-    return match || IncidentTypes.Default;
-  },
-  [GroupByOptions.ZipCode]: (t) => t.derived.zip,
   [GroupByOptions.TimeInterval]: ({ derived: { timestamp } }) => {
     const duration = DefaultInterval;
     const end = 1590448143000; // TODO lol
@@ -56,45 +30,11 @@ export const Mappers = {
 };
 
 export function groupBy(option = GroupByOptions.Nothing, tweets) {
-  if (option === GroupByOptions.Nothing) {
-    return byNothing(tweets);
-  }
-  if (option === GroupByOptions.IncidentType) {
-    return byIncidentType(tweets);
-  }
-  if (option === GroupByOptions.ZipCode) {
-    return byZip(tweets);
-  }
   if (option === GroupByOptions.TimeInterval) {
     return byTimeInterval(tweets);
   }
   throw `Unrecognized groupby option: ${option}`;
 }
-
-const byNothing = (tweets) => {
-  return [
-    {
-      groupby: undefined,
-      key: undefined,
-      values: tweets,
-    },
-  ];
-};
-
-const byIncidentType = (tweets) => {
-  const option = GroupByOptions.IncidentType;
-  const grouped = by(GroupByOptions.IncidentType, tweets, RequiredKeys[option]);
-
-  const color = scaleOrdinal(schemeCategory10);
-  const { Fire, Medic, Aid } = IncidentTypes.Known;
-  color.domain([Medic, IncidentTypes.Default, Aid, Fire]); // https://github.com/d3/d3-scale-chromatic#schemeCategory10
-
-  return grouped.map((d) => ({ ...d, color: color(d.key) }));
-};
-
-const byZip = (tweets) => {
-  return by(GroupByOptions.ZipCode, tweets);
-};
 
 const byTimeInterval = (tweets) => {
   return by(GroupByOptions.TimeInterval, tweets, []);
