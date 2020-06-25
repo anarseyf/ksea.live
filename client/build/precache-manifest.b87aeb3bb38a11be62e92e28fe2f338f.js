a688d09d2070788a44313a608616d6dd9006c08d
Anar Seyf <anar.seyf@gmail.com>
2020-06-25 16:12:08 -0700

    webpack-bundle-analyzer (fail)

--- client/build/precache-manifest.b87aeb3bb38a11be62e92e28fe2f338f.js
+++ client/build/precache-manifest.b87aeb3bb38a11be62e92e28fe2f338f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4368a99ab75e4df7aee587ab2d800254",
+    "revision": "522a522482b66872070d0ca50ced242f",
     "url": "/index.html"
   },
   {
-    "revision": "db37f9bc2f31ffc1e68c",
+    "revision": "756e3d38e507bfdd3878",
     "url": "/static/css/2.daf24185.chunk.css"
   },
   {
-    "revision": "0437a328942a07822c46",
+    "revision": "3e515aba8e05219b573c",
     "url": "/static/css/main.0051ebe9.chunk.css"
   },
   {
-    "revision": "db37f9bc2f31ffc1e68c",
-    "url": "/static/js/2.49883fc7.chunk.js"
+    "revision": "756e3d38e507bfdd3878",
+    "url": "/static/js/2.92618a09.chunk.js"
   },
   {
     "revision": "6791e7fb9584d65ed87d655035c7da95",
-    "url": "/static/js/2.49883fc7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.92618a09.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0437a328942a07822c46",
-    "url": "/static/js/main.52be4600.chunk.js"
+    "revision": "3e515aba8e05219b573c",
+    "url": "/static/js/main.0e291b1d.chunk.js"
   },
   {
     "revision": "0e35831b56fb2c044e12",
