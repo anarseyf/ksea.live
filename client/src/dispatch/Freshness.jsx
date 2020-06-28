097636e9f98697957be9fa0cb62be8228b2cd5af
Anar Seyf <anar.seyf@gmail.com>
2020-06-28 11:22:53 -0700

    Staleness

--- client/src/dispatch/Freshness.jsx
+++ client/src/dispatch/Freshness.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { StatusContext } from "./StatusContext";
-import { timeFormatter, timeFormatterUserTimezone } from "../clientUtils";
+import { timeFormatter, timeFormatterUserTimezone, pacificMidnight } from "../clientUtils";
+import classnames from "classnames";
 import styles from "./freshness.module.scss";
 
 export const Freshness = () => {
@@ -11,8 +12,10 @@ export const Freshness = () => {
   }
 
   const date = new Date(status.lastUpdated);
-  const formatTimezone = "h:mma z";
-  const formatNoTimezone = "h:mma";
+  const midnight = pacificMidnight();
+  const stale = date < midnight;
+  const formatTimezone = `${stale ? "MMM D @ " : ""}h:mma z`;
+  const formatNoTimezone = `${stale ? "MMM D @ " : ""}h:mma`;
   const seattle = timeFormatter(date, formatNoTimezone);
   const seattleTZ = timeFormatter(date, formatTimezone);
   const user = timeFormatterUserTimezone(date, formatNoTimezone);
@@ -25,8 +28,10 @@ export const Freshness = () => {
   return (
     <span>
       Data is current as of{" "}
-      <span className={styles.time}>{displaySeattle}</span>
-      {displayUser}.
+      <span className={classnames({[styles.stale]: stale})}>
+        <span className={styles.time}>{displaySeattle}</span>
+        {displayUser}.
+      </span>
     </span>
   );
 };
--- client/src/dispatch/Topline.jsx
+++ client/src/dispatch/Topline.jsx
@@ -1,20 +1,34 @@
-import React from "react";
-import { timeFormatterMonthDay } from "../clientUtils";
+import React, { useContext, useEffect, useState } from "react";
+import { timeFormatterMonthDay, pacificMidnight } from "../clientUtils";
 import styles from "./topline.module.scss";
 import { isPhone } from "../clientUtils";
 import classnames from "classnames";
+import { DataContext } from './DataProvider';
 
 export const Topline = ({ total, text }) => {
+  const { status } = useContext(DataContext);
   const isDefined = typeof total === "number";
   const totalContent = isDefined ? total : "⋯";
   const totalElement = <div className={styles.total}>{totalContent}</div>;
   const phone = isPhone();
+  const [stale, setStale] = useState(false);
+  const [lastUpdated, setLastUpdated] = useState(<span>&nbsp;</span>);
+
+  useEffect(() => {
+    if (status.lastUpdated) {
+      const date = new Date(status.lastUpdated);
+      const midnight = pacificMidnight();
+      setLastUpdated(timeFormatterMonthDay(+date));
+      setStale(date < midnight);
+    }
+  }, [setLastUpdated, status]);
+  
 
   return (
     <div className={classnames(styles.topline, {[styles.phone]: phone})}>
       {text && <div className={styles.text}>{text}</div>}
       {totalElement}
-      <div className={styles.date}>{timeFormatterMonthDay(new Date())}</div>
+      <div className={classnames(styles.date, { [styles.stale]: stale })}>{lastUpdated}</div>
     </div>
   );
 }
