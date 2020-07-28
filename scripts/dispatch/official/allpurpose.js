3aa1204d661ab8c9d9cbab72a6960b48c7383a24
Anar Seyf <anar.seyf@gmail.com>
2020-07-27 17:37:50 -0700

    Remove derived from files

--- scripts/dispatch/official/allpurpose.js
+++ scripts/dispatch/official/allpurpose.js
@@ -1,21 +1,11 @@
-import moment from "moment";
-import { tz } from "moment-timezone";
+import { modifyAll } from "./scriptUtil";
 
-const str = "6/11/2020 5:36:08 PM";
-console.log(str);
+const main = async () => {
+  const f = ({ derived, ...rest }) => ({
+    ...rest,
+    ...derived,
+  });
+  modifyAll(f);
+};
 
-const format = "MM/DD/YYYY hh:mm:ss A";
-const timezone = "America/Vancouver";
-const m1 = tz(str, format, timezone);
-const m2 = tz(str, format, "UTC");
-
-console.log(m1.toDate().toLocaleString());
-console.log(m2.toDate().toLocaleString());
-
-console.log(moment.tz.names());
-console.log(
-  moment.tz
-    .names()
-    .filter((v) => v.includes("America"))
-    .join("\n")
-);
+main();
